b075b728b5b3143860b62dff727912742dd0973f
YersonDevelop <yersonquiber2014@gmail.com>
2023-08-25 11:32:10 -0500

    JsonXml

---
+++ FirstJsonXml/src/main/java/co/edu/ue/FirstJsonXmlApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJsonXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJsonXmlApplication.class, args);
+	}
+
+}
